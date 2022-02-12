91c2ed458b5a9826ea41d14264678a35bd1b8993
Piash Islam <piashislam2010@gmail.com>
2022-02-12 22:26:24 +0600

    lecture1

---
+++ bin/practice.dart
@@ -0,0 +1,28 @@
+void main(){
+  int userId= 218108;
+  String userName= "Shoel";
+  double userAge= 27.5;
+  /*bool userMaridStatus= true;
+  List list =[1,2,3];
+  Map map={"Name":"Sohel","Age":"23"};
+
+   */
+  // print("Name:$userName  Id:$userId  Age:$userAge");
+  // print("Name:$userName  \nId:$userId  \nAge:$userAge");
+  // print("Name:$userName  \nId:$userId  \nAge:$userAge");
+
+
+
+  int value1=130;
+  int value2=130;
+
+  int sum=value1+value2;
+  int sub=value1-value2;
+  double div=value1/value2;
+  int mul=value1*value2;
+  print("$value1+$value2=$sum\n");
+  print("$value1-$value2=$sub\n");
+  print("$value1*$value2=$mul\n");
+  print("$value1/$value2=$div");
+
+}
